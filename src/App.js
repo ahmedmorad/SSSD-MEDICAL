333f3904301aac2e23b40e95eebb846fce7178af
ahmedmorad101 <ahmedmorad101@gmail.com>
2017-06-29 21:27:01 +0400

    apply routers

--- src/App.js
+++ src/App.js
@@ -1,41 +1,30 @@
-import React, { Component } from 'react';
-import { Menu, Container, Button, Checkbox, Form, Header, Segment, Icon, Divider, Grid } from 'semantic-ui-react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {Container} from 'semantic-ui-react';
 import './style.css';
-import CheckupList from './containers/checkup-list';
-import CheckupDetail from './containers/checkup-details';
+// import CheckupList from './containers/checkup-list'; import CheckupDetail
+// from './containers/checkup-details';
 import AppMenu from './components/menu';
 import Signin from './components/auth/signin';
 import Signup from './components/auth/signup';
+import Home from './components/home';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        
-        <AppMenu></AppMenu>
-        <Container>
 
-          <Grid columns={2} stackable={true} divided>
-            <Grid.Row>
-              <Grid.Column>
-                  <Signin/>
-              </Grid.Column>
-              <Grid.Column>
-                <Signup/>
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-              <Grid.Column width={4}>
-                <CheckupList/>
-              </Grid.Column>
-              <Grid.Column  width={12}>
-                <CheckupDetail/>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Container>
+        <Router>
+          <div>
+            <AppMenu></AppMenu>
+            <Container>
+              <Route path="/" exact={true} component={Home}/>
+              <Route path="/signin" component={Signin}/>
+              <Route path="/signup" component={Signup}/>
+            </Container>
+          </div>
+        </Router>
 
       </div>
     );
--- src/components/menu.js
+++ src/components/menu.js
@@ -1,29 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {Menu} from 'semantic-ui-react';
 import logo from '../logo.svg';
 import '../style.css';
-
+import { Link} from 'react-router-dom';
+// import {BrowserRouter as Router, Route} from 'react-router-dom';
 class AppMenu extends Component {
 
-    handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+    handleItemClick = (e, {name}) => this.setState({activeItem: name});
 
     render() {
         return (
             <Menu>
                 <Menu.Menu>
-                    <Menu.Item name='logo' onClick={this.handleItemClick}>Logo <img src={logo} className="NavBar-logo" alt="logo" /></Menu.Item>
-                    <Menu.Item name='patients' onClick={this.handleItemClick}>Patients</Menu.Item>
+                    <Menu.Item name='logo' as={Link} to='/'>
+                        Logo
+                        <img src={logo} className="NavBar-logo" alt="logo"/>
+                    </Menu.Item>
+
+                    <Menu.Item name='patients' >Patients</Menu.Item>
+
                 </Menu.Menu>
 
-
                 <Menu.Menu position='right'>
-                    <Menu.Item name='signin' onClick={this.handleItemClick}>Sign In</Menu.Item>
-                    <Menu.Item name='signup' onClick={this.handleItemClick}>Sign up</Menu.Item>
+                    <Menu.Item name='signin' as={Link} to='/signin'>Sign In</Menu.Item>
+                    <Menu.Item name='signup' as={Link} to='/signup'>Sign up</Menu.Item>
                 </Menu.Menu>
             </Menu>
         );
     }
 }
-
 
 export default AppMenu;
